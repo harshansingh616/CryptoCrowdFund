57436f0ce794c6aee39cd46a62ababbefce8ea56
harshansingh616 <95460292+harshansingh616@users.noreply.github.com>
2022-10-12 17:34:38 +0530

    provider changes

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
           connectors={{
             walletconnect: {
               rpcUrl:
-                "https://rinkeby.infura.io/v3/08ac79d88b5d4aea961ca36af7ea6ee7",
+              "https://goerli.infura.io/v3/f2ff8590ac4446adb158e0fc6ad8349d",
             },
           }}
         >
